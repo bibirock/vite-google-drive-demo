9302b909da9d4b1085de7c2cb92e194b3392b03a
Ben.chen <ben.chen@keyxentic.com>
2022-12-03 00:12:54 +0800

    refactor: 將i18n全域方法改寫至main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,29 @@
 import { createPinia } from 'pinia';
 import router from './router';
-import CustomIcon from '@/components/customIcon/CustomIcon.vue';
-import { Icon } from '@iconify/vue';
+import './index.css';
+
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
+
+import App from './App.vue';
+import { createApp } from 'vue';
+const app = createApp(App);
+
+//全域變數設置
+import { globalFunction, TYPE } from './globalMethods/global';
 import { i18n } from './locales/lang.js';
+const { t } = i18n.global;
 import mitt from 'mitt';
 const emitter = mitt();
-import { globalFunction, TYPE } from './globalMethods/global';
-import './index.css';
-import { createApp } from 'vue';
-import App from './App.vue';
 
-const app = createApp(App);
-//全域變數設置
-app.provide('$emitter', emitter);
 app.provide('$globalF', globalFunction);
+app.provide('$emitter', emitter);
 app.provide('$TYPE', TYPE);
+app.provide('$t', t);
+
 //全域組件
+import CustomIcon from '@/components/customIcon/CustomIcon.vue';
+import { Icon } from '@iconify/vue';
 app.component('CustomIcon', CustomIcon).component('Icon', Icon);
 app.use(router).use(createPinia()).use(i18n).use(Antd).mount('#app');
 
