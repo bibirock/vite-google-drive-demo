61cc1d3d36b1ab4d36703559598e87653d6bac9e
Ben.Chen <ben.chen@keyxentic.com>
2022-12-15 17:51:34 +0800

    feat: global.ts型別定義完成

--- src/globalMethods/global.ts
+++ src/globalMethods/global.ts
@@ -1,22 +1,23 @@
 //如果想在非setup函式中使用router，不能使用useRouter 要使用單獨引入的方式
-import router from '@/router';
+import type { drive_v3 } from '@googleapis/drive/v3';
 import { emitter } from '@/main';
+import router from '@/router';
 
 export const globalFunction = {
-    openFileView: (viewLink: string) => {
-        window.open(viewLink, '_blank');
+    openFileView: (viewLink: drive_v3.Schema$File['webViewLink']) => {
+        window.open(viewLink as string, '_blank');
     },
 
-    goToFolder: (folderId: string) => {
+    goToFolder: (folderId: drive_v3.Schema$File['id']) => {
         router.push({ name: 'my-drive-folders', params: { folderId: folderId } });
     },
 
-    sendFileDatil: (mataData: string | undefined) => {
+    sendFileDatil: (mataData: drive_v3.Schema$File | undefined) => {
         emitter.emit('send-file-data', mataData);
     },
 
-    copyLink: (link: string) => {
-        return navigator.clipboard.writeText(link);
+    copyLink: (link: drive_v3.Schema$File['webViewLink']) => {
+        return navigator.clipboard.writeText(link as string);
     },
 };
 
